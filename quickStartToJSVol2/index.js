7f202cb8da1508f8eb527454cd90e08b0da7047f
Won Maung Thein <wonmaung.thein@bbc.co.uk>
2019-03-04 14:11:39 +0000

    Added a new folder and file

---
+++ quickStartToJSVol2/index.js
@@ -0,0 +1,15 @@
+const name = "Won";
+
+switch (name) {
+  case "Lion":
+    console.log("This is Lion");
+    break;
+  case "elephant":
+    console.log("This is elephant");
+    break;
+  case "Won":
+    console.log("My name is " + name);
+    break;
+  default:
+    console.log("This is the king");
+}
